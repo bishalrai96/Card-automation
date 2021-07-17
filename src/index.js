ff8544deadbdbefa08c4f5abc0df0170107a82ca
bishalrai96 <bishal.rai64@Yahoo.com>
2021-07-17 10:25:00 +0100

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ try {
     const project = core.getInput('project');
     const column = core.getInput('column');
     const octokit = github.getOctokit(token);
-    console.log("the github context ", github.context)
+    // console.log("the github context ", github.context)
+
+    // get url
+    const { eventName, payload } = githubContext;
+    console.log("eventName", eventName);
+    console.log("payload", payload);
+
     // with is not getting executed in
 } catch (error) {
     console.log("failed but why ", error.message)
