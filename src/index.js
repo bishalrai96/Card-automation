6410ab08bde6c8a377c32edcb46c1282510b0530
bishalrai96 <bishal.rai64@yahoo.com>
2021-07-17 10:29:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ try {
     const { eventName, payload } =  github.context;
     console.log("eventName", eventName);
     console.log("payload", payload);
-    console.log(typeof, github.context);
+    console.log(typeof github.context);
 
     // with is not getting executed in
 } catch (error) {
