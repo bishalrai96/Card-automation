c01ff89e3c34916bf064047a44ab1ec913394114
bishalrai96 <bishal.rai64@Yahoo.com>
2021-07-19 22:34:44 +0100

    aa

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ try {
 
         var labelIsPresent = true;
 
-        if (payload.issue.label.name == label) {
+        if (payload.label.name == label) {
             labelIsPresent = true;
         }
         /*
